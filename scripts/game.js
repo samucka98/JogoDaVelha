e201c1357d9500c48f77a452d220453b0a24a1cb
samuel <samuelibiapinolima@gmail.com>
2020-05-16 12:18:23 -0300

    completo

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,62 @@
 const squares = ['', '', '', '', '', '', '', '', ''];
-const playerTime = 0;
+let playerTime = 0;
+const statesWin = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+let gameOver = false;
+
 const symbols = ['o', 'x'];
 
 function handleMove(position) {
-  console.log(position);
+
+  if (!gameOver) {
+    if (squares[position] === '') {
+      squares[position] = symbols[playerTime];
+
+      gameOver = isWin();
+
+      if (gameOver) {
+        setTimeout(() => {
+          alert(`O jogador ${playerTime} venceu!`);
+        }, 10);
+      }
+
+      updateStage(squares[position], position);
+
+      playerTime = (playerTime == 0) ? 1 : 0;
+    }
+  }
+}
+
+function isWin() {
+  for (let i = 0; i < statesWin.length; i++) {
+    let pos = statesWin[i];
+
+    let pos1 = pos[0];
+    let pos2 = pos[1];
+    let pos3 = pos[2];
+
+    if (squares[pos1] == squares[pos2] && squares[pos1] == squares[pos3] && squares[pos1] !== '') {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function resetGame() {
+  playerTime = 0;
+  gameOver = false;
+
+  for (let i = 0; i < squares.length; i++) {
+    squares[i] = '';
+    clearStage(squares[i], i);
+  }
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   const squares = document.querySelectorAll('.square');
 
   squares.forEach((square) => {
     square.addEventListener('click', handleClick);
   })
+
+  const btnReset = document.getElementById("reset");
+  btnReset.addEventListener('click', newGame);
 });
 
 function handleClick(event) {
   const position = event.target.id;
+
   handleMove(position);
+}
+
+function updateStage(square, position) {
+  document.getElementById(`${position}`).innerHTML = `<div class="${square}"></div>`;
+}
+
+function clearStage(square, position) {
+  document.getElementById(`${position}`).innerHTML = '';
+}
+
+function newGame() {
+  resetGame();
 }
